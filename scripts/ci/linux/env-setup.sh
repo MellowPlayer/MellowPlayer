7c402a065c4e1b83fe8e5c6cc76d1f8fef818c97
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2018-11-25 14:58:08 +0000

    Update env-setup.sh

--- scripts/ci/linux/env-setup.sh
+++ scripts/ci/linux/env-setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 apt update
-apt install $(grep -vE "^\s*#" scripts/ci/linux/requirements.txt  | tr "\n" " ")
+apt install $(grep -vE "^\s*#" scripts/ci/linux/requirements.txt  | tr "\n" " ") -y
