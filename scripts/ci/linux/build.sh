63f4a07cc89132e81aeb8147b756feb69f71dfcb
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2018-12-01 21:42:50 +0100

    Moving to gitlab
    CI Setup: improve deploy:linux stage to reuse build:linux artifact

--- scripts/ci/linux/build.sh
+++ scripts/ci/linux/build.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+mkdir -p appdir
 mkdir -p build && cd build
-cmake -DBUILD_TESTS=ON ..
-cmake --build . --config Release -- -j $(grep -c ^processor /proc/cpuinfo)
+
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON -DSTATIC_LIBSTDCPP=TRUE ..
+make -j$(nproc)
+make DESTDIR=../appdir install
--- scripts/packaging/make_appimage.sh
+++ scripts/packaging/make_appimage.sh
@@ -22,30 +22,12 @@ echo "downloading linuxdeployqt"
 wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt*.AppImage
 
-# Build MellowPlayer and link statically git libstdc++
-mkdir -p build
-mkdir -p appdir
-pushd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=FALSE -DSTATIC_LIBSTDCPP=TRUE ..
-make -j$(nproc)
-make DESTDIR=../appdir install
-popd
-
 # create appdir
 ./linuxdeployqt*.AppImage ./appdir/usr/share/applications/*.desktop -exclude-libs="libnss3.so,libnssutil3.so" -bundle-non-qt-libs -qmldir=src/lib/presentation/imports/MellowPlayer -verbose=2
 
 # copy missing qml files
 echo "Copying missing files..."
 cp ${QT_DIR}/plugins/imageformats/libqsvg.so ./appdir/usr/plugins/imageformats/
-mkdir -p ./appdir/usr/qml
-cp -R ${QT_DIR}/qml ./appdir/usr/qml
-echo "ls ./appdir/usr/qml"
-ls ./appdir/usr/qml
-echo "ls /usr/share/qt5/resources"
-ls /usr/share/qt5/resources
-cp -R /usr/share/qt5 ./appdir/usr
-echo "ls ./appdir/usr/resources"
-ls ./appdir/usr/resources
 
 # create appimage
 ./linuxdeployqt*.AppImage ./appdir/usr/share/applications/*.desktop -exclude-libs="libnss3.so,libnssutil3.so" -appimage
