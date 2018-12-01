4e980ab578a774b0388e17ce3f42916a75ee171c
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2018-12-01 22:31:24 +0100

    Moving to gitlab
    CI Setup: fix coverage stage

--- scripts/ci/linux/coverage.sh
+++ scripts/ci/linux/coverage.sh
@@ -4,5 +4,5 @@ set -e
 
 mkdir -p build && cd build
 cmake -DBUILD_TESTS=ON -DENABLE_COVERAGE=ON -DENABLE_LCOV_REPORT=ON -DCMAKE_BUILD_TYPE=Debug ..
-cmake --build . --config DEBUG -- -j $(grep -c ^processor /proc/cpuinfo)
-xvfb-run cmake --build . --config DEBUG --target coverage
+cmake --build . --config DEBUG -- -j$(nproc)
+cmake --build . --config DEBUG --target coverage
