0d3e2bf8897b4b773f73252f2cae31b31dea2da6
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2018-12-02 12:50:15 +0100

    Moving to gitlab
    Windows CI Setup using appveyor

--- scripts/packaging/windows/make_installer.bat
+++ scripts/packaging/windows/make_installer.bat
@@ -4,9 +4,9 @@ set PATH=%QTDIR%\bin;%QTDIR%\lib;%PATH%;
 set BUILD_DIR=build
 
 mkdir %BUILD_DIR%\bin
-mkdir %BUILD_DIR%\bin\plugins
+mkdir %BUILD_DIR%\bin\plugins\web
 xcopy /S /C /Y %BUILD_DIR%\src\main\Release\MellowPlayer.exe %BUILD_DIR%\bin
-xcopy /S /E /D /C /Y plugins %BUILD_DIR%\bin\plugins
+xcopy /S /E /D /C /Y src/plugins/web %BUILD_DIR%\bin\plugins\web
 
 %QTDIR%\bin\windeployqt %BUILD_DIR%\bin\MellowPlayer.exe -qmldir=src
 
