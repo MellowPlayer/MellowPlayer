40ef9dd5e7ddbe29f92c93c47b7ccb36ff155a83
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2018-12-26 13:23:17 +0100

    Fix upload stable release scripts

--- scripts/ci/windows/upload-stable.bat
+++ scripts/ci/windows/upload-stable.bat
@@ -1,4 +1,3 @@
 cd ..\..\..
 
-curl -T .\MellowPlayer_Setup.exe -ucolinduquesnoy:%BINTRAY_API_KEY% "https://api.bintray.com/content/colinduquesnoy/MellowPlayer/Stable/%APPVEYOR_BUILD_VERSION%/stable/%APPVEYOR_BUILD_VERSION%/MellowPlayer_Setup.exe?publish=1&override=1&explode=0"
-curl -T .\MellowPlayer_Setup.exe -ucolinduquesnoy:%BINTRAY_API_KEY% "https://api.bintray.com/content/colinduquesnoy/MellowPlayer/Stable/%APPVEYOR_BUILD_VERSION%/stable/latest/MellowPlayer_Setup.exe?publish=1&override=1&explode=0"
+curl -T .\MellowPlayer_Setup.exe -ucolinduquesnoy:%BINTRAY_API_KEY% "https://api.bintray.com/content/colinduquesnoy/MellowPlayer/Stable/%APP_VERSION%/stable/%APP_VERSION%/MellowPlayer_Setup.exe?publish=1&override=1&explode=0"
