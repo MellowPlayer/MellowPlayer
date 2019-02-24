80747b413aaa4f5b90fde1a894a77f7fc4baca5a
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2019-02-24 15:02:53 +0100

    ci: simplify make-flatpak

--- scripts/packaging/make-flatpak.sh
+++ scripts/packaging/make-flatpak.sh
@@ -3,7 +3,6 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 flatpak install -y flathub org.kde.Sdk/x86_64/5.12
 flatpak install -y flathub org.kde.Platform/x86_64/5.12
 
-flatpak-builder _build com.gitlab.ColinDuquesnoy.MellowPlayer.yml
 flatpak-builder --repo=repo --force-clean _build com.gitlab.ColinDuquesnoy.MellowPlayer.yml
 
 flatpak build-bundle repo MellowPlayer.flatpak com.gitlab.ColinDuquesnoy.MellowPlayer
