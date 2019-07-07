74faf5ec2aa51b75303fb0551c3f0ea9eecfaccd
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2019-07-07 15:29:49 +0200

    CI: use own docker image for building flatpak
    This should solve the 502 issue we often have when building the flatpak

--- scripts/ci/linux/fedora-flatpak-env-setup.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-dnf install -y flatpak flatpak-builder
--- scripts/packaging/make-flatpak.sh
+++ scripts/packaging/make-flatpak.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install -y flathub org.kde.Sdk/x86_64/5.12
-flatpak install -y flathub org.kde.Platform/x86_64/5.12
-
 flatpak-builder --repo=repo --force-clean _build com.gitlab.ColinDuquesnoy.MellowPlayer.yml
-
 flatpak build-bundle repo MellowPlayer.flatpak com.gitlab.ColinDuquesnoy.MellowPlayer
