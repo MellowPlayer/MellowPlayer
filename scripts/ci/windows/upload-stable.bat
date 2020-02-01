4bc44bbdb450b5455f9c5422592654aa488f89f4
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2020-02-01 16:11:55 +0100

    Don't use build number in stable releases, only in continuous builds

--- scripts/ci/windows/upload-stable.bat
+++ scripts/ci/windows/upload-stable.bat
@@ -1,6 +1,3 @@
 cd ..\..\..
 
-git rev-list HEAD --count > build_number.txt
-set /p build_number=<build_number.txt
-
-curl -T .\MellowPlayer_Setup.exe -ucolinduquesnoy:%BINTRAY_API_KEY% "https://api.bintray.com/content/colinduquesnoy/MellowPlayer/Stable/%APP_VERSION%.%build_number%/stable/%APP_VERSION%.%build_number%/MellowPlayer_Setup.exe?publish=1&override=1&explode=0"
+curl -T .\MellowPlayer_Setup.exe -ucolinduquesnoy:%BINTRAY_API_KEY% "https://api.bintray.com/content/colinduquesnoy/MellowPlayer/Stable/%APP_VERSION%/stable/%APP_VERSION%/MellowPlayer_Setup.exe?publish=1&override=1&explode=0"
