449bcf092f5e994008659fd924d8e85895d7a6ba
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2020-09-21 16:22:11 +0200

    Improve install-widevine.sh script to install plugin in our flatpak

--- scripts/install-widevine.sh
+++ scripts/install-widevine.sh
@@ -16,7 +16,6 @@ cd $tmp_dir
 #set urls and checksums
 url1="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 file1=$(basename "$url1")
-sha256_1=("229b35f0d41bbb6edd98ce4ab8305994a0f5cd1ac4d9817571f07365b2d1ad80")
 
 #Download Files
 wget $url1 -P $tmp_dir
@@ -29,3 +28,8 @@ tar -xvf $tmp_dir/data.tar.xz --strip-components 4 ./opt/google/chrome/WidevineC
 echo "Installing libwidevinecdm.so to /usr/lib/chromium"
 sudo mkdir -p /usr/lib/chromium
 sudo install -Dm644 $tmp_dir/WidevineCdm/_platform_specific/linux_x64/libwidevinecdm.so -t /usr/lib/chromium
+
+if [ -d "/var/lib/flatpak/app/com.gitlab.ColinDuquesnoy.MellowPlayer/current/active/files/bin" ]; then
+  echo "Installing libwidevinecdm.so for use in flatpak"
+  sudo cp /usr/lib/chromium/libwidevinecdm.so /var/lib/flatpak/app/com.gitlab.ColinDuquesnoy.MellowPlayer/current/active/files/bin
+fi
