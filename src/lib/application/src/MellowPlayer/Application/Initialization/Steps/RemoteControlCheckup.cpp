f89d7418266cc9b291595d21f9330c0e9c32959e
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2020-09-21 14:31:14 +0200

    [MellowPlayer.Connect Integration] Don't block application if failed to download archive
    Happened while a new release was available but artifacts were not already uploaded

--- src/lib/application/src/MellowPlayer/Application/Initialization/Steps/RemoteControlCheckup.cpp
+++ src/lib/application/src/MellowPlayer/Application/Initialization/Steps/RemoteControlCheckup.cpp
@@ -25,7 +25,7 @@ void RemoteControlCheckup::initialize(const ResultCallback& resultCallback)
         LOG_DEBUG(_logger, _remoteControlApplication.name() << " is outdated");
         _remoteControlApplication.install([=](bool success, const QString& errorMessage) {
             _errorMessage = "Failed to install " + _remoteControlApplication.name() + ": " + errorMessage;
-            resultCallback(success);
+            resultCallback(true);
         });
         return;
     }
