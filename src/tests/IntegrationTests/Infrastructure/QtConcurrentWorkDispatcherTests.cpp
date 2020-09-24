eb27473f79f6d3282a0f8c9cc22de0a78166226d
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2020-09-24 20:23:46 +0200

    #464 Technical stack update
    Try to find failing integration test

---
+++ src/tests/IntegrationTests/Infrastructure/QtConcurrentWorkDispatcherTests.cpp
@@ -0,0 +1,29 @@
+#include <MellowPlayer/Infrastructure/System/QtConcurrentWorkDispatcher.hpp>
+#include <QtTest/qtestsystem.h>
+#include <catch/catch.hpp>
+#include <thread>
+
+using namespace std::this_thread;
+using namespace std::chrono;
+using namespace MellowPlayer::Infrastructure;
+
+TEST_CASE("QtConcurrentWorkDispatcherTests")
+{
+    QtConcurrentWorkDispatcher workDispatcher;
+
+    SECTION("invoke")
+    {
+        bool finished = false;
+        workDispatcher.invoke([&]() { finished = true; });
+        sleep_for(milliseconds(500));
+        REQUIRE(finished);
+    }
+
+    SECTION("delayInvoke")
+    {
+        bool finished = false;
+        workDispatcher.delayInvoke(100, [&]() { finished = true; });
+        QTest::qWait(1000);
+        REQUIRE(finished);
+    }
+}
