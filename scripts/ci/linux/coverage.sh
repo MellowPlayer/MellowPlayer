14f1ed2bd26a753b00b7a5a540e5fffc9bf70a4e
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2020-09-24 22:21:02 +0000

    Update coverage.sh

--- scripts/ci/linux/coverage.sh
+++ scripts/ci/linux/coverage.sh
@@ -5,5 +5,5 @@ cd build
 
 ctest
 lcov --directory . --capture --output-file coverage.info
-lcov --remove coverage.info '*tests/*' '*.local/*' '*QQmlObjectListModel*' '*Qt5*/*' '*I*.hpp' '*Qt/5.*' '*3rdparty/*' '*qrc_*' '*moc_*' '/usr/*' '/opt/*' --output-file coverage.info.cleaned
+lcov --remove coverage.info '*/_deps/*' '*tests/*' '*.local/*' '*QQmlObjectListModel*' '*Qt5*/*' '*I*.hpp' '*Qt/5.*' '*3rdparty/*' '*qrc_*' '*moc_*' '/usr/*' '/opt/*' --output-file coverage.info.cleaned
 lcov --list coverage.info.cleaned
