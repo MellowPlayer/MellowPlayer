6d5f1994325ca9a8c7ce7d6dc62b78b2c897a71f
Sibren Vasse <gitlab@sibrenvasse.nl>
2021-05-19 13:06:40 +0200

    Fixup code style

--- src/plugins/tidal/integration.js
+++ src/plugins/tidal/integration.js
@@ -1,78 +1,79 @@
 function getTitle() {
     try {
         return document.querySelector('[data-test="footer-track-title"]').children[0].children[0].innerText;
-    } catch(e) {
-        return "";
+    } catch (e) {
+        return '';
     }
 }
 
 function getSongId() {
-	var songName = getTitle();
-	if (songName != "") {
-		return getHashCode(songName);
-	}
-	return ""
+    var songName = getTitle();
+    if (songName != '') {
+        return getHashCode(songName);
+    }
+    return '';
 }
 
 function getArtist() {
     try {
-        return document.querySelector("#footerPlayer [data-test='grid-item-detail-text-title-artist']" ).innerText;
-    } catch(e) {
-        return "";
+        return document.querySelector('#footerPlayer [data-test="grid-item-detail-text-title-artist"]').innerText;
+    } catch (e) {
+        return '';
     }
 }
 
 function getAlbumTitle() {
     try {
         return document.querySelector('[data-test="left-column-footer-player"] [class*="container"] a').innerText;
-    } catch(e) {
-        return "";
+    } catch (e) {
+        return '';
     }
 }
 
 function getArtUrl() {
-	try {
+    try {
         return document.querySelector('[data-test="current-media-imagery"] img').src;
-    } catch(e) {
-        return "";
+    } catch (e) {
+        return '';
     }
 }
 
 function getPosition() {
     try {
         position = document.querySelector('[data-test="current-time"]').innerText;
-    } catch(e) {
-        return 0
+    } catch (e) {
+        return 0;
     }
-    return toSeconds(position)
+    return toSeconds(position);
 }
 
 function getDuration() {
     try {
         var time = document.querySelector('[data-test="duration-time"]').innerText;
-    } catch(e) {
-        return 0
+    } catch (e) {
+        return 0;
     }
-    return toSeconds(time)
+    return toSeconds(time);
 }
 
 function isFavorite() {
     try {
-        return document.querySelector('[data-test="footer-favorite-button"]').attributes['aria-checked'].value === "true";
+        return document.querySelector('[data-test="footer-favorite-button"]').attributes['aria-checked'].value === 'true';
     } catch (e) {
         return false;
     }
 }
 
 function getPlaybackStatus() {
-    if (document.querySelector('[data-test="pause"]'))
-      return MellowPlayer.PlaybackStatus.PLAYING;
-    else if (document.querySelector('[data-test="play"]'))
-      return MellowPlayer.PlaybackStatus.PAUSED;
-//    else if (document.querySelector('[data-test="connecting"]'))
+    if (document.querySelector('[data-test="pause"]')) {
+        return MellowPlayer.PlaybackStatus.PLAYING;
+    } else if (document.querySelector('[data-test="play"]')) {
+        return MellowPlayer.PlaybackStatus.PAUSED;
+//    } else if (document.querySelector('[data-test="connecting"]')) {
 //      return MellowPlayer.PlaybackStatus.BUFFERING;
-    else
+    } else {
       return MellowPlayer.PlaybackStatus.STOPPED;
+    }
 }
 
 function update() {
@@ -90,24 +91,24 @@ function update() {
         "artistName": getArtist(),
         "albumTitle": getAlbumTitle(),
         "artUrl": getArtUrl(),
-        "isFavorite": isFavorite()
+        "isFavorite": isFavorite(),
     };
 }
 
 function play() {
-	document.querySelector('[data-test="play"]').click();
+    document.querySelector('[data-test="play"]').click();
 }
 
 function pause() {
-	document.querySelector('[data-test="pause"]').click();
+    document.querySelector('[data-test="pause"]').click();
 }
 
 function goNext() {
-	document.querySelector('[data-test="next"]').click();
+    document.querySelector('[data-test="next"]').click();
 }
 
 function goPrevious() {
-	document.querySelector('[data-test="previous"]').click();
+    document.querySelector('[data-test="previous"]').click();
 }
 
 function setVolume(volume) {
@@ -115,11 +116,11 @@ function setVolume(volume) {
 }
 
 function addToFavorites() {
-  document.querySelector('[data-test="footer-favorite-button"]').click();
+    document.querySelector('[data-test="footer-favorite-button"]').click();
 }
 
 function removeFromFavorites() {
-  document.querySelector('[data-test="footer-favorite-button"]').click();
+    document.querySelector('[data-test="footer-favorite-button"]').click();
 }
 
 function seekToPosition(position) {
